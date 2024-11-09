4e5b95282839413d2fa905383073578fb5e6072f
Stefan Adams <stefan@adams.fm>
2024-11-09 13:56:06 -0600

    ...

--- lib/Mojolicious/Plugin/SmartLog.pm
+++ lib/Mojolicious/Plugin/SmartLog.pm
@@ -5,6 +5,7 @@ use Time::HiRes qw(time);
 
 has level => undef;
 has stderr => 0;
+has user => sub { sub ($c) {} };
 
 sub register ($self, $app, $conf) {
   # Note: logging to stdout/stderr will not get picked up by journald when run with hypnotoad
@@ -14,25 +15,26 @@ sub register ($self, $app, $conf) {
   $app->log->path($app->home->child('log', $app->mode . '.log')) if -d $app->home->child('log');
   $self->level($conf->{level} || $app->config->{log}{level});
   $app->log->level($self->level) if $self->level;
+  my $time = time;
   $app->log->on(message => sub ($log, $level, @lines) {
-    warn $log->format->(time, $level, @lines) ;
-    # if ($log->path && $self->stderr) {
-    #   warn ($conf->{log_time} || $app->config->{log}{log_time}) ? $log->format->(time, $level, @lines) : sprintf "[%s] %s\n", $level, join "\n", @lines;
-    # }
+    if ($log->path && $self->stderr) {
+      warn (($conf->{log_time} || $app->config->{log}{log_time}) ? $log->format->(time, $level, @lines) : sprintf("[%.3f] [%s] %s\n", time - $time, $level, join " ", @lines));
+    }
     return if grep { /Error handling errors/ } @lines; # don't handle errors if there's an error handling errors
     return unless grep { /Raptor (Rainbow|Not Found) Shown/ } @lines; # only handle errors if there's a rainbow or not found shown
     # Handle raptor problems, such as sending a special alert or logging to a different file
-    ...
+    # ...
   });
   $app->hook(before_render => sub ($c, $args) {
     return unless my $template = $args->{template};
+    my $user = $self->user->($c) || '-';
     if ($template eq 'exception') { # Make sure we are rendering the exception template
       return unless my $e = $c->stash->{exception};
       return unless my $snapshot = $c->stash->{snapshot};
-      $c->log->error(sprintf '[%s] [%s] Raptor Rainbow Shown: %s (%s#%s); (%d) %s', $args->{status}, $c->user||'-', $c->req->url, $snapshot->{controller}, $snapshot->{action}, $e->line->[0], $e);
+      $c->log->error(sprintf '[%s] [%s] Raptor Rainbow Shown: %s (%s#%s); (%d) %s', $args->{status}, $user, $c->req->url, $snapshot->{controller}, $snapshot->{action}, $e->line->[0], $e);
     }
     elsif ($template eq 'not_found') { # Make sure we are rendering the not_found template
-      $c->log->warn(sprintf '[%s] [%s] Raptor Not Found Shown: %s', $args->{status}, $c->user||'-', $c->req->url);
+      $c->log->warn(sprintf '[%s] [%s] Raptor Not Found Shown: %s', $args->{status}, $user, $c->req->url);
     }
   });
 }
